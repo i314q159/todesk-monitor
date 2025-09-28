391047c84508ca8478f87b414432d4b7e1671231
i314q159 <i314q159@outlook.com>
2025-09-28 12:23:54 +0800

    update

--- scripts/todesk_monitor.sh
+++ scripts/todesk_monitor.sh
@@ -12,13 +12,11 @@ current_time=$(date "+%Y-%m-%d %H:%M:%S")
 service_status=$(systemctl is-active $SERVICE_NAME)
 if [ "$service_status" != "active" ]; then
     echo "[$current_time] 警告: $SERVICE_NAME 服务未运行！状态: $service_status" >> "$LOG_FILE"
-    # 尝试重启服务
     systemctl restart $SERVICE_NAME
 fi
 
 # 使用pgrep检查进程是否存在todesk gui(Linux系统)
-# if ! pgrep -x "$PROCESS_NAME" > /dev/null; then
-#     # 如果进程不存在，记录到日志文件
-#     echo "[$current_time] 警告: $PROCESS_NAME 进程未运行！" >> "$LOG_FILE"
-#     systemctl restart $SERVICE_NAME
-# fi
+if ! pgrep -x "$PROCESS_NAME" > /dev/null; then
+    echo "[$current_time] 警告: $PROCESS_NAME 进程未运行！" >> "$LOG_FILE"
+    todesk &
+fi
